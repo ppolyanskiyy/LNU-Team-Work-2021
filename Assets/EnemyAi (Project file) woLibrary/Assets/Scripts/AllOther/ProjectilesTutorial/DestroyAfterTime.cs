6b29ba0ede89d00e1b4b6e83ed04c9cd334e6b91
DimaDiD <didiakivskyy@gmail.com>
2021-04-20 09:19:45 +0300

    add health bar for player and enemies

---
+++ Assets/EnemyAi (Project file) woLibrary/Assets/Scripts/AllOther/ProjectilesTutorial/DestroyAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    public ParticleSystem momentBurst;
+    public float timer;
+
+    void Start()
+    {
+        Debug.Log("Start");
+        momentBurst.playOnAwake = true;
+        Invoke("Destroy", timer);
+    }
+    private void Destroy()
+    {
+        Debug.Log("ENDDD");
+        Destroy(gameObject);
+    }
+}
