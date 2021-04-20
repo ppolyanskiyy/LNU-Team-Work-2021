f768a8dffd6deae0c986128f593c887fef5fb2f5
loburyaka <borysko2002@gmail.com>
2021-04-20 18:25:49 +0300

    Imported multiplayer online engine

---
+++ Assets/MultiplayerOnline/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game.");
+        Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void WhatsNew()
+    {
+        SceneManager.LoadScene("What\'s new");
+    }
+}
