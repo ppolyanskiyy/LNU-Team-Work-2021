519f7626e216453bf39cea36a468e70360b988c1
SwaTiK4931 <40461618+SwaTiK4931@users.noreply.github.com>
2021-04-30 14:21:29 +0300

    Improved aiming animation when firing

--- Assets/Player/Scripts/PlayerAiming.cs
+++ Assets/Player/Scripts/PlayerAiming.cs
@@ -6,14 +6,17 @@ using UnityEngine.Animations.Rigging;
 public class PlayerAiming : MonoBehaviour
 {
     [SerializeField] float turnSpeed = 15;
-    Camera mainCamera;
     [SerializeField] Rig aimLayer;
 
+    float waitAfterFire = 2f;
+    float timeAfterFire;
+    Camera mainCamera;
     float aimDuration = 0.2f;
     RaycastWeapon weapon;
 
     void Start()
     {
+        timeAfterFire = 0f;
         mainCamera = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
         weapon = GetComponentInChildren<RaycastWeapon>();
@@ -27,11 +30,15 @@ public class PlayerAiming : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (timeAfterFire > waitAfterFire)
+        {
+            timeAfterFire = waitAfterFire;
+        }
+        if (Input.GetMouseButton(1) || Input.GetMouseButton(0))
         {
             aimLayer.weight += Time.deltaTime / aimDuration;
         }
-        else
+        else if (timeAfterFire >= waitAfterFire)
         {
             aimLayer.weight -= Time.deltaTime / aimDuration;
         }
@@ -44,10 +51,16 @@ public class PlayerAiming : MonoBehaviour
         {
             weapon.UpdateFiring(Time.deltaTime);
         }
+        else
+        {
+            timeAfterFire += Time.deltaTime;
+        }
         weapon.UpdateBullets(Time.deltaTime);
         if (Input.GetMouseButtonUp(0))
         {
+            timeAfterFire = 0f;
             weapon.StopFiring();
         }
+        Debug.Log(timeAfterFire);
     }
 }
